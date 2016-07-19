98482988ace0b28f00f58fe21245d9b94d323ab1
ArcherFromGrodno <ArcherFromGrodno@gmail.com>
2016-07-19 03:06:06 +0300

    The test application to set likes by hashtags was implemented.

--- InstagramApp/InstagramApp/Program.cs
+++ InstagramApp/InstagramApp/Program.cs
@@ -29,7 +29,48 @@ namespace InstagramApp
             Thread.Sleep(500);
 
             IList<IWebElement> buttons = driver.FindElements(By.TagName("button"));
-            buttons.First(element => element.Text == "Войти").Click();
+            buttons.FirstOrDefault(element => element.Text == "Войти").Click();
+
+            Thread.Sleep(2000);
+
+            IList<IWebElement> inputs = driver.FindElements(By.ClassName("_9x5sw"));
+            inputs.FirstOrDefault().SendKeys("#Grodno");
+
+            Thread.Sleep(3000);
+
+            IList<IWebElement> hashLinks = driver.FindElements(By.ClassName("_k2vj6"));
+            hashLinks.FirstOrDefault().Click();
+
+            Thread.Sleep(3000);
+            IList<IWebElement> images = driver.FindElements(By.ClassName("_8mlbc"));
+
+            foreach (var image in images)
+            {
+                Thread.Sleep(500);
+                image.Click();
+
+                Thread.Sleep(500);
+
+                IList<IWebElement> likeSpans = driver.FindElements(By.ClassName("_1tv0k"));
+                var text = likeSpans.FirstOrDefault().Text;
+
+                Thread.Sleep(500);
+
+                if (text == "Нравится")
+                {
+                    IList<IWebElement> likeButtons = driver.FindElements(By.ClassName("_ebwb5"));
+                    likeButtons.FirstOrDefault().Click();
+                }
+                else
+                {
+                    driver.Keyboard.SendKeys(Keys.Escape);
+                    break;
+                }
+
+                Thread.Sleep(500);
+
+                driver.Keyboard.SendKeys(Keys.Escape);
+            }
         }
     }
 }
