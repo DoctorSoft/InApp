b0cd2227ca0a9e571f01209f2bc178e863760f5e
DoctorSoft <megopixel91@gmail.com>
2016-09-06 21:31:05 +0300

    add engine and service AddComment

---
+++ InstagramApp/Engines/Engines/AddCommentEngine/AddCommentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Engines.Engines.AddCommentEngine
+{
+    public class AddCommentModel
+    {
+        public string Link { get; set; }
+        public string CommentText { get; set; }
+
+    }
+}
