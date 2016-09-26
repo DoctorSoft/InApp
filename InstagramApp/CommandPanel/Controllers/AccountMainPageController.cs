788a8c1b785e3b8098b6c18b44f69253c95e8cd3
ArcherFromGrodno <ArcherFromGrodno@gmail.com>
2016-09-26 21:40:59 +0300

    The link to move to page of account data was implemented.

---
+++ InstagramApp/CommandPanel/Controllers/AccountMainPageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Constants;
+
+namespace CommandPanel.Controllers
+{
+    public class AccountMainPageController : Controller
+    {
+        // GET: AccountMainPage
+        public ActionResult Index(AccountName accountId)
+        {
+            return View(accountId);
+        }
+    }
+}
