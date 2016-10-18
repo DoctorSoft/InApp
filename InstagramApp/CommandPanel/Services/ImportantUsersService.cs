703980ae7e5e29bec5119fd261cb83b668a90338
ArcherFromGrodno <ArcherFromGrodno@gmail.com>
2016-10-18 23:44:23 +0300

    The functionality to manage important users was implemented.

---
+++ InstagramApp/CommandPanel/Services/ImportantUsersService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommandPanel.Models.ImportantUserModels;
+using CommandPanel.Models.LanguageModels;
+using Constants;
+using DataBase.Factories;
+using DataBase.QueriesAndCommands.Commands.Users;
+using DataBase.QueriesAndCommands.Queries.Languages;
+using DataBase.QueriesAndCommands.Queries.Users;
+
+namespace CommandPanel.Services
+{
+    public class ImportantUsersService
+    {
+        public List<ImportantUserViewModel> GetImportantUsers(AccountName accountId)
+        {
+            var context = new ContextFactory().GetContext(accountId);
+
+            var users = new GetUsersImpotantForOwnerQueryHandler(context).Handle(new GetUsersImpotantForOwnerQuery { MaxCount = int.MaxValue });
+
+            return users.Select(s => new ImportantUserViewModel
+            {
+                UserLink = s
+            }).ToList();
+        }
+
+        public void RemoveImportantUser(AccountName accountId, string user)
+        {
+            var context = new ContextFactory().GetContext(accountId);
+
+            new RemoveUserCommandHandler(context).Handle(new RemoveUserCommand
+            {
+                UserLink = user
+            });
+        }
+
+        public void AddImportantUser(AccountName accountId, string user)
+        {
+            var context = new ContextFactory().GetContext(accountId);
+
+            new MarkUserAsImportantCommandHandler(context).Handle(new MarkUserAsImportantCommand
+            {
+                UserLink = user
+            });
+        }
+    }
+}
---
+++ InstagramApp/DataBase/QueriesAndCommands/Commands/Users/MarkUserAsImportantCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity.Migrations;
+using System.Linq;
+using Constants;
+using DataBase.Contexts;
+using DataBase.Models;
+using DataBase.QueriesAndCommands.Common;
+
+namespace DataBase.QueriesAndCommands.Commands.Users
+{
+    public class MarkUserAsImportantCommandHandler : ICommandHandler<MarkUserAsImportantCommand, VoidCommandResponse>
+    {
+        private readonly DataBaseContext context;
+
+        public MarkUserAsImportantCommandHandler(DataBaseContext context)
+        {
+            this.context = context;
+        }
+
+        public VoidCommandResponse Handle(MarkUserAsImportantCommand command)
+        {
+            var user = context.Users.FirstOrDefault(model => model.Link == command.UserLink);
+
+            if (user == null)
+            {
+                user = new UserDbModel
+                {
+                    Link = command.UserLink,
+                    UserStatus = UserStatus.ImportantForOwner
+                };
+            }
+            else
+            {
+                user.UserStatus = UserStatus.ImportantForOwner;
+            }
+
+            context.Users.AddOrUpdate(user);
+            context.SaveChanges();
+
+            return new VoidCommandResponse();
+        }
+    }
+}
