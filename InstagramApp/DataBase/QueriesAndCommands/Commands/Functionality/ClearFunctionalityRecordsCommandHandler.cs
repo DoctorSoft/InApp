44017bf6b6910099a263e8ab76be90cbfd795ea9
ArcherFromGrodno <ArcherFromGrodno@gmail.com>
2016-10-21 01:12:31 +0300

    The functionality to clear logs was implemented.

---
+++ InstagramApp/DataBase/QueriesAndCommands/Commands/Functionality/ClearFunctionalityRecordsCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using DataBase.Contexts;
+using DataBase.QueriesAndCommands.Common;
+using EntityFramework.Extensions;
+
+namespace DataBase.QueriesAndCommands.Commands.Functionality
+{
+    public class ClearFunctionalityRecordsCommandHandler : ICommandHandler<ClearFunctionalityRecordsCommand, VoidCommandResponse>
+    {
+        private readonly DataBaseContext context;
+
+        public ClearFunctionalityRecordsCommandHandler(DataBaseContext context)
+        {
+            this.context = context;
+        }
+
+        public VoidCommandResponse Handle(ClearFunctionalityRecordsCommand command)
+        {
+            context.FunctionalityRecords.Delete();
+            context.SaveChanges();
+
+            return new VoidCommandResponse();
+        }
+    }
+}
